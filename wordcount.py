dd187883b5fe529c77283c40d0ce8dc3de96f49a
Dmitrii Altukhov <dm.altukhov@ya.ru>
2020-04-20 19:36:53 +0300

    Convert wordcount.py to python3

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python -tt
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -47,22 +46,24 @@ import sys
 
 ###
 
+
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
-  if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
-    sys.exit(1)
+    if len(sys.argv) != 3:
+        print("usage: ./wordcount.py {--count | --topcount} file")
+        sys.exit(1)
 
-  option = sys.argv[1]
-  filename = sys.argv[2]
-  if option == '--count':
-    print_words(filename)
-  elif option == '--topcount':
-    print_top(filename)
-  else:
-    print 'unknown option: ' + option
-    sys.exit(1)
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == "--count":
+        print_words(filename)
+    elif option == "--topcount":
+        print_top(filename)
+    else:
+        print("unknown option: " + option)
+        sys.exit(1)
 
-if __name__ == '__main__':
-  main()
+
+if __name__ == "__main__":
+    main()
