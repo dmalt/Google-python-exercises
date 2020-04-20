f5ebd4f014556fcf672104a1ee12fac77f50646e
Dmitrii Altukhov <dm.altukhov@ya.ru>
2020-04-20 19:01:36 +0300

    Remove sorting exercise from list1.py

--- list1.py
+++ list1.py
@@ -37,17 +37,6 @@ def front_x(words):
     return
 
 
-# C. sort_last
-# Given a list of non-empty tuples, return a list sorted in increasing
-# order by the last element in each tuple.
-# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
-# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
-# Hint: use a custom key= function to extract the last element form each tuple.
-def sort_last(tuples):
-    # +++your code here+++
-    return
-
-
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
